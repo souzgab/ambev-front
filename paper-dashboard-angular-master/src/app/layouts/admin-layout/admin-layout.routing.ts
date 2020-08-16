801c5d13ce43cf3a4202082a620f0a04b3337f74
unknown <gs_arruda@outlook.com>
2020-08-16 15:39:19 -0300

    update stuff for 2b ambev

---
+++ paper-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserComponent } from '../../pages/user/user.component';
+import { TableComponent } from '../../pages/table/table.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { NotificationsComponent } from '../../pages/notifications/notifications.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user',           component: UserComponent },
+    { path: 'table',          component: TableComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+];
